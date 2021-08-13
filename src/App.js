ea4fa2a6a629e8f4c7dac2bc96ad944e6f94dc88
naumanashraf12 <Fa18-bcs-200@cuilahore.edu.pk>
2021-08-13 20:30:55 +0500

    adas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Nav from "./Nav";
+import Main from "./Main.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
